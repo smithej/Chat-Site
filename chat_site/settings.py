a32d0698d0be91004596bcf62d7f2c5abb75fc25
Evan Smith <esmith@perfectserve.net>
2019-12-04 09:39:58 -0500

    Serve chat from ASGI

--- chat_site/settings.py
+++ chat_site/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'chat',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Channels
+ASGI_APPLICATION = 'chat_site.routing.application'
